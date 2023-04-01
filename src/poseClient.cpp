40ef801118989115a87481fd09e5276d5e9af9f0
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 <feesposito00@gmail.com>
2023-04-01 12:33:49 +0200

    first commit

---
+++ src/poseClient.cpp
@@ -0,0 +1,32 @@
+#include "ros/ros.h" 
+#include "cwork4/getRelativePose.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Pose.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "poseClinet");
+    ros::NodeHandle n;
+
+    ros::ServiceClient client = n.serviceClient<cwork4::getRelativePose>("getRelativePose");
+
+    cwork4::getRelativePose srv;
+    srv.request.ref_frame.data = "/base_link";
+    srv.request.frame.data = "/camera_link";
+
+    cout<<srv.request.ref_frame.data<<endl<<srv.request.frame.data<<endl;
+    ROS_INFO("Waiting for the client server\n");
+
+	client.waitForExistence();
+	ROS_INFO("Client server up now\n");
+
+    if (!client.call(srv)) {
+        ROS_ERROR("Error calling the service");
+        return 1;
+    }
+
+    ROS_INFO("All good!\n");
+
+    return 0;
+}
---
+++ src/poseServer.cpp
@@ -0,0 +1,39 @@
+#include "ros/ros.h" 
+#include "cwork4/getRelativePose.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Pose.h"
+#include "tf/transform_listener.h"
+
+using namespace std;
+
+bool service_callback(cwork4::getRelativePose::Request &req, cwork4::getRelativePose::Response &res) {
+    bool working_fine = true;
+
+    tf::TransformListener listener;
+    tf::StampedTransform transform;
+
+    cout<<req.ref_frame.data<<endl<<req.frame.data<<endl;
+
+    try {
+        listener.lookupTransform(req.ref_frame.data, req.frame.data, ros::Time(0), transform);
+        working_fine = true;    
+        //ROS_INFO(transform);
+    } catch (tf::TransformException ex) {
+        ROS_ERROR("%s", ex.what());
+        working_fine = false;
+    }
+
+    return working_fine;
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "poseServer");
+    ros::NodeHandle n;
+
+    ros::ServiceServer service = n.advertiseService("getRelativePose", service_callback);
+
+    ros::spin();
+
+    return 0;
+}
